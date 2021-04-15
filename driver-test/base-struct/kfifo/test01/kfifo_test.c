0227222da74d9ca695bd85c4a528be6b06bea915
baiyang <baiyang0223@163.com>
2021-04-15 16:32:37 +0800

    [KFIFO]: 测试kfifo得越界行为

--- driver-test/base-struct/kfifo/test01/kfifo_test.c
+++ driver-test/base-struct/kfifo/test01/kfifo_test.c
@@ -1,15 +1,16 @@
 /*************************************************************************
-    > File Name: kfifo_test.c
-    > Author: baiy
-    > Mail: baiyang0223@163.com 
-    > Created Time: 2020-12-04-17:19:54
-    > Func:  测试内核KFIFO
+	> File Name: kfifo_test.c
+	> Author: baiy
+	> Mail: baiyang0223@163.com
+	> Created Time: 2020-12-04-17:19:54
+	> Func:  测试内核KFIFO
  ************************************************************************/
 #define pr_fmt(fmt) "[%s:%d:]" fmt, __func__, __LINE__
 #include <linux/uaccess.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/delay.h>
 #include <linux/notifier.h>
 #include <linux/init.h>
 #include <linux/types.h>
@@ -17,9 +18,9 @@
 #include <linux/kthread.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE > KERNEL_VERSION(3, 3, 0)
-    #include <asm/switch_to.h>
+	#include <asm/switch_to.h>
 #else
-    #include <asm/system.h>
+	#include <asm/system.h>
 #endif
 
 #include <linux/proc_fs.h>
@@ -33,7 +34,7 @@ static struct task_struct *push_thread = NULL;
 static struct task_struct *pop_thread = NULL;
 
 /* fifo size in elements (bytes) */
-#define FIFO_SIZE   32
+#define FIFO_SIZE	8
 #ifdef DYNAMIC
 static struct kfifo test_fifo;
 #else
@@ -43,46 +44,50 @@ static DECLARE_KFIFO(test_fifo, unsigned char, FIFO_SIZE);
 
 static int make_kfifo_push_thread(void *data)
 {
-     int i = 5;
+	 int i = 20;
 	 int size = 0;
-     struct completion cmpl;
-     unsigned char val = 0;
-     printk("[KFIFO PUSH]thread starting...\n");
-     while((i--) > 0){
-            init_completion(&cmpl);
-            wait_for_completion_timeout(&cmpl, 3 * HZ);
-			size = kfifo_len(&test_fifo);
-			if(size > MAX_FIFO_COUNT) {
-            	printk("[KFIFO PUSH]: size %d,skip push \n", size);
+	 struct completion cmpl;
+	 unsigned char val = 0;
+	 printk("[KFIFO PUSH]thread starting...\n");
+	 while((i--) > 0){
+			init_completion(&cmpl);
+			wait_for_completion_timeout(&cmpl, 1 * HZ);
+			if (kfifo_is_full(&test_fifo)) {
+				printk("[KFIFO PUSH]: size %#x,skip push \n", size);
 				continue;
 			}
-            get_random_bytes(&val,sizeof(val));  //生成一个内核随机数
+			get_random_bytes(&val,sizeof(val));  //生成一个内核随机数
 			kfifo_put(&test_fifo, val);
-            printk("[KFIFO PUSH]: %u, size %d \n",val, size);
-     }
-     printk("[KFIFO PUSH] ended!\n");
-     return 0;
+			size = kfifo_len(&test_fifo);
+			printk("[KFIFO PUSH]: %u, size %#x , in %#x, out %#x, len %#x\n",
+				val, size, test_fifo.kfifo.in, test_fifo.kfifo.out,test_fifo.kfifo.in - test_fifo.kfifo.out );
+	 }
+	 printk("[KFIFO PUSH] ended!\n");
+	 return 0;
 }
+
 static int make_kfifo_pop_thread(void *data)
 {
-     int i = 100;
 	 int size = 0;
-     struct completion cmpl;
-     unsigned char val = 0;
-     printk("[KFIFO POP]thread starting...\n");
-     while((i--) > 0){
-            init_completion(&cmpl);
-            wait_for_completion_timeout(&cmpl, 1 * HZ);
+	 struct completion cmpl;
+	 unsigned char val = 0;
+	 printk("[KFIFO POP]thread starting...\n");
+	 while( 1 ){
+            msleep(1000);
+			init_completion(&cmpl);
+			wait_for_completion_timeout(&cmpl, 5 * HZ);
 			size = kfifo_len(&test_fifo);
-			if(size <= 0) {
-            	printk("[KFIFO POP]: size %d,skip pop \n", size);
-				continue;
+			if (kfifo_is_empty(&test_fifo)) {
+				printk("[KFIFO POP]: size %#x,skip pop,exit \n", size);
+                break;
 			}
 			kfifo_get(&test_fifo, &val);
-            printk("[KFIFO POP]: %u, size %d \n",val, size);
-     }
-     printk("[KFIFO POP] ended!\n");
-     return 0;
+			size = kfifo_len(&test_fifo);
+			printk("[KFIFO POP]: %u, size %#x , in %#x, out %#x, len %#x\n",
+				val, size, test_fifo.kfifo.in, test_fifo.kfifo.out, test_fifo.kfifo.in - test_fifo.kfifo.out );
+	 }
+	 printk("[KFIFO POP] ended!\n");
+	 return 0;
 }
 
 
@@ -90,19 +95,23 @@ static int __init kfifo_dev_init(void)
 {
 #ifdef DYNAMIC
 	int ret;
-
+	pr_info("Used dynamic kfifo");
 	ret = kfifo_alloc(&test_fifo, FIFO_SIZE, GFP_KERNEL);
 	if (ret) {
 		printk(KERN_ERR "error kfifo_alloc\n");
 		return ret;
 	}
 #else
+	pr_info("Used Static kfifo");
 	INIT_KFIFO(test_fifo);
 #endif
-	pr_info("kfifo init");
+	pr_info("kfifo init, kfifo mask is %#x", test_fifo.kfifo.mask);
+	test_fifo.kfifo.in = ~0;
+	test_fifo.kfifo.out = (~0) - 1;
+
 	push_thread = kthread_run(make_kfifo_push_thread,NULL,"kfifo push");
 	pop_thread = kthread_run(make_kfifo_pop_thread,NULL,"kfifo pop");
-    return 0;
+	return 0;
 }
 
 static void __exit kfifo_dev_exit(void)
@@ -118,4 +127,4 @@ module_init(kfifo_dev_init);
 module_exit(kfifo_dev_exit);
 
 MODULE_LICENSE("GPL v2");
-MODULE_INFO(supported, "Test driver that simulate serial port over PCI");
+MODULE_INFO(supported, "Test kfifo");
