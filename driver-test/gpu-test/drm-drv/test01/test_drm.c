51d2cc89a0aa9ec9d884bd53025bc7c416dde379
baiyang <baiyang0223@163.com>
2021-05-02 16:56:51 +0800

    [DRV]:增加一个最简单的drm驱动

--- driver-test/gpu-test/drm-drv/test01/test_drm.c
+++ driver-test/gpu-test/drm-drv/test01/test_drm.c
@@ -1,12 +1,16 @@
 /*************************************************************************
-    > File Name: test_drm.c
-    > Author: baiy
-    > Mail: baiyang0223@163.com 
-    > Created Time: 2021-05-01-23:24:35
-    > Func: 
+	> File Name: test_drm.c
+	> Author: baiy
+	> Mail: baiyang0223@163.com 
+	> Created Time: 2021-05-01-23:24:35
+	> Func: 
  ************************************************************************/
+#define pr_fmt(fmt)  "[%s:%d] " fmt, __func__, __LINE__
 
 #include <drm/drmP.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_gem.h>
+#include <drm/drm_vblank.h>
 
 static struct drm_device *vkms_dev;
 
@@ -14,14 +18,20 @@ static struct drm_driver vkms_drv = {
 	.name			= "vkms",
 	.desc			= "Virtual Kernel Mode Setting",
 	.date			= "20180514",
-	.major			= 1,
-	.minor			= 0,
 };
 
 static int __init vkms_init(void)
 {
-	drm_dev_alloc(&vkms_drv, NULL);
+	pr_info("[E]");
+	vkms_dev = drm_dev_alloc(&vkms_drv, NULL);
+	if (!vkms_dev) {
+		pr_err("[vkms] dev alloc failed");
+		return -ENOMEM;
+	}
+
+	pr_info("[vkms] dev register");
 	drm_dev_register(vkms_dev, 0);
+	pr_info("[X]\n");
 
 	return 0;
 }
@@ -29,10 +39,12 @@ static int __init vkms_init(void)
 
 static void __exit vkms_exit(void)
 {
+	pr_info("[E]");
 	if (vkms_dev) {
 		drm_dev_unregister(vkms_dev);
 		drm_dev_put(vkms_dev);
 	}
+	pr_info("[X]\n");
 }
 
 module_init(vkms_init);
